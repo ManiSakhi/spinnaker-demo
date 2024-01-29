8acbccbf7d7045e14e2d918d1c8e7b88bc642889
ManiSakhi <96877629+ManiSakhi@users.noreply.github.com>
2024-01-29 18:49:04 +0530

    Update 4-configure-oauth.sh

--- spinnaker-data/scripts/4-configure-oauth.sh
+++ spinnaker-data/scripts/4-configure-oauth.sh
@@ -4,7 +4,7 @@
 CLIENT_ID=93ae7696433a31f6c2a2
 CLIENT_SECRET=29e4c57025fa873685c81d7c17b830c80bfb0fa3
 PROVIDER=github
-
+REDIRECT_URI=http://54.88.91.13:9000
 set -e
 
 if [ -z "${CLIENT_ID}" ] ; then
@@ -19,6 +19,12 @@ if [ -z "${PROVIDER}" ] ; then
   echo "PROVIDER not set"
   exit
 fi
+fi
+if [ -z "${REDIRECT_URI}" ] ; then
+ echo "REDIRECT_URI not set"
+  exit
+fi
+
 
 MY_IP=`curl -s ifconfig.co`
 
@@ -27,5 +33,7 @@ hal config security authn oauth2 edit \
   --client-secret $CLIENT_SECRET \
   --provider $PROVIDER
 hal config security authn oauth2 enable
+
+hal config security authn oauth2 edit --pre-established-redirect-uri $REDIRECT_URI
 
 
